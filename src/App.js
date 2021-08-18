ada3665f0daf080b873cdf78e9ef8010658d22df
Ala Abu Rish <alaa.rish2@gmail.com>
2021-08-19 01:09:01 +0300

    lab6-update

--- src/App.js
+++ src/App.js
@@ -14,18 +14,20 @@ class App extends React.Component {
           lon:''
         }
       ],
-      showData: false
+      showData: false,
+      mapLink: ''
     }
   }
 
   getLocation = async (event)=>{
     event.preventDefault();
    
-    let resultData = await axios.get(`https://eu1.locationiq.com/v1/search.php?key=pk.f06c34f17ca6b04d7f4dae5995fa4ca1&q=${event.target.cityName.value}&format=json`);
+    let resultData = await axios.get(`https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${event.target.cityName.value}&format=json`);
 
     await this.setState({
       resultData: resultData.data[0],
       showData: true,
+      mapLink: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${resultData.data[0].lat},${resultData.data[0].lon}&zoom=18`
     })
   }
 
@@ -45,6 +47,8 @@ class App extends React.Component {
        <p>display_name: {this.state.resultData.display_name}</p>
        <p>latitude: {this.state.resultData.lat}</p>
        <p>longitude: {this.state.resultData.lon}</p>
+       <br/>
+       <img src={this.state.mapLink} alt='map img' style={{width: '300px'}}/>
      </div>
    </div>
   );
