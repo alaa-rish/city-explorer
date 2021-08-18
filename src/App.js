622cfdf77d05824dd6e2f0f6507ab2dc5e86b1cf
Ala Abu Rish <alaa.rish2@gmail.com>
2021-08-19 00:53:45 +0300

    lab6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react';
+import axios from 'axios';
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state ={
+      resultData: [
+        {
+          display_name: '',
+          lat: '',
+          lon:''
+        }
+      ],
+      showData: false
+    }
+  }
+
+  getLocation = async (event)=>{
+    event.preventDefault();
+   
+    let resultData = await axios.get(`https://eu1.locationiq.com/v1/search.php?key=pk.f06c34f17ca6b04d7f4dae5995fa4ca1&q=${event.target.cityName.value}&format=json`);
+
+    await this.setState({
+      resultData: resultData.data[0],
+      showData: true,
+    })
+  }
+
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     <form onSubmit={this.getLocation}>
+       <fieldset>
+         <input type="text" placeholder="City" name="cityName"/>
+       </fieldset>
+       <fieldset>
+         <button>Explor!</button>
+       </fieldset>
+     </form>
+     <br/>
+     <div showData={this.state.showData}>
+       <p>display_name: {this.state.resultData.display_name}</p>
+       <p>latitude: {this.state.resultData.lat}</p>
+       <p>longitude: {this.state.resultData.lon}</p>
+     </div>
+   </div>
   );
+  }
 }
 
 export default App;
